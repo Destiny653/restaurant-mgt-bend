d4d73612913d05f2b4a1d333d523a3937607db51
Destiny653 <fokundem.com@gmail.com>
2025-02-06 08:13:38 +0100

    server mordification

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 const app = express();
 
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => {
+app.use(cors())
 app.use(express.json());
 
 // Import routes
@@ -25,8 +29,46 @@ app.use("/api/owner", ownerRoutes);
 app.use("/api/auth", authRoute);
 app.use("/api/customer", customerRoute);
 
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => {
+app.use((req, res, next)=>{
+  return res.status(404).json({
+      status: 'NOT FOUND',
+      status_code: 404,
+      message: 'The requested resource was not found',
+      data:{
+          protocol: req.protocol,
+          method: req.method.toUpperCase(),
+          url: req.originalUrl,
+          path: req.path,
+          query: req.query,
+          ip: req.ip, 
+          host: req.hostname,  
+          port: req.port,
+          timestamp: new Date()
+      }
+  })
+})
+
+app.use((error, req, res, next)=>{
+  return res.status(500).json({
+      status: 'ERROR',
+      status_code: error.status || 500,
+      message: error.message,
+      data:{
+          protocol: req.protocol,
+          method: req.method.toUpperCase(),
+          error: error.stack,
+          url: req.originalUrl,
+          path: req.path,
+          query: req.query,  
+          ip: req.ip,
+          host:req.hostname, 
+          port: req.port,
+          timestamp: new Date()
+      }
+  })
+});
+
+
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
     console.log("Database connected")
