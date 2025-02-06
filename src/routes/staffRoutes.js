505dae143fb6bca598f72a1af1a158864fc234f2
Destiny653 <fokundem.com@gmail.com>
2025-02-06 12:46:04 +0100

    routing from customer to staff

--- src/routes/staffRoutes.js
+++ src/routes/staffRoutes.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 const staffController = require("../controllers/staffController");
 const { authenticate, authorize } = require("../middlewares/authMiddleware");
+const reservation = require("../controllers/reservationController")
+
 
 router.post("/login", staffController.loginStaff)
+router.post("/register", reservation.registerCustomer) 
 router.get("/reservations", authenticate, authorize(["Owner"]), staffController.getAllReservations);
 router.post("/add", authenticate, authorize(["Owner"]), staffController.addStaff);
 router.put("/:id", authenticate, authorize(["Owner"]), staffController.modifyStaff);
